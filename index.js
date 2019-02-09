748caeacb39ab4aa942c806e0d339f76a67a1be4
kaushgem <8935010+kaushgem@users.noreply.github.com>
2019-02-09 00:56:28 -0500

    undo

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 var express = require('express')
 var app = express()
+var email = require('./email.js')
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! This is awesome!!!!')
+  response.send('Hello World! This is awesomeoooo awesome!!!!')
 })
 
 app.get('/contact', function(request, response) {
-  response.redirect(301, 'http://google.com');
+  response.redirect(301, 'http://google.com')
+})
+
+app.get('/email', function(request, response) {
+  response.send('Mail Sent!')
+  email.sendMail()
+})
+
+app.get('/kaush', function(request, response) {
+  response.redirect(301, 'https://github.com/kaushgem')
 })
 
 app.listen(app.get('port'), function() {
